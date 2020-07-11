f74063ec88dd1ca13e1bcaf863c453a4f822e34d
thomas <thomas.chang@hotmail.ca>
2020-07-11 19:54:04 -0400

    search string

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,1 +1,6 @@
 package main
+
+func Search(dictionary map[string]string, word string) string {
+
+	return dictionary[word]
+}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,1 +1,14 @@
 package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "string of text"}
+
+	got := Search(dictionary, "test")
+	want := "string of text"
+
+	if got != want {
+		t.Errorf("got %q instead of %q", got, want)
+	}
+}
